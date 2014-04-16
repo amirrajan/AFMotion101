0bd0a018833fc0f54f76fe8749874ba6cb728a6e
Amir Rajan <ar@amirrajan.net>
2014-04-16 11:01:50 -0500

    using module directly

--- client/spec/sample_server_spec.rb
+++ client/spec/sample_server_spec.rb
@@ -2,12 +2,7 @@ describe "sample service" do
   it "works" do
     @sample_servce = SampleService.new
     @sample_servce.get
-    wait
+    RunLoopHelpers::proper_wait 0.1
     @sample_servce.result.should.not.equal nil
-  end
-
-
-  def wait sec = 0.1
-    CFRunLoopRunInMode(KCFRunLoopDefaultMode, sec, false)
   end
 end
